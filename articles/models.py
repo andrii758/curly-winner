363e499ca2a329d97c1f26672ebc18f896959080
oriviao <ponomarenko.a@proton.me>
2026-02-17 07:29:07 +0200

    created html's templates and a model for article.

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-class Article(models.Model):
-    title = CharField(max_length=50)
-    body = TextField()
 
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Article
 
@@ -7,3 +7,8 @@ from .models import Article
 class HomePageView(ListView):
     model = Article
     template_name = "home.html"
+
+
+class DetailPageView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
